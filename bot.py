b5ab541e01c07a0420750d238b16efbbd8a447d0
Streetovertake <164423191+Streetovertake@users.noreply.github.com>
2026-05-09 14:28:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
-import telebot
-import os
+import telebot import os
 
 TOKEN = os.getenv("TOKEN")
 
+if not TOKEN: raise Exception("TOKEN не найден в переменных окружения")
+
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, "Бот работает 🚀")
+@bot.message_handler(commands=['start']) def start(message): bot.send_message( message.chat.id, "Привет 👋\nБот работает 24/7 🚀" )
+
+@bot.message_handler(commands=['help']) def help_cmd(message): bot.send_message( message.chat.id, "Команды:\n/start - запуск\n/help - помощь" )
 
 bot.infinity_polling()
